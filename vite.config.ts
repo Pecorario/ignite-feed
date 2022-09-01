75e712391c4c8d3c8e2927b4f55c96055737b669
Taynara Pecorario <taynara_rp22@hotmail.com>
2022-09-01 18:05:20 -0300

    Fixing vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ignite-feed/',
   plugins: [react()],
   server: {
     port: 3001
